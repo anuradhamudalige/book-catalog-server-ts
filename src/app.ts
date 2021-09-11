f04a066c7993893893a6849d2f4f8f5658576b38
Anuradha Mudalige <anuradamudalige@gmail.com>
2021-09-12 01:14:59 +0530

    CORS correction for deploy

--- src/app.ts
+++ src/app.ts
@@ -8,7 +8,7 @@ const port = process.env.PORT || 1337;
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+    res.header("Access-Control-Allow-Origin", "https://anuradhamudalige.github.io/book-catalog-app/");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
